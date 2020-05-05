eba34b2f223663483615db3537fcb442c3ba72b1
Bhawna Parihar <52083996+Bhawnaparihar@users.noreply.github.com>
2020-05-05 18:58:13 +0530

    Add files via upload

--- LinqAssignment/Program.cs
+++ LinqAssignment/Program.cs
@@ -145,6 +145,99 @@ namespace LinqAssignment
             {
                 Console.WriteLine($"ID : {emp.Employee_Id} Name : {emp.First_Name} {emp.Last_Name} Salary : {emp.Salary} Joining Date : {emp.Joining_Date} Department : {emp.Department}");
             }
+            var sn = Employee.GetEmployees().Where(e => e.First_Name == "John" || e.First_Name == "Michael");
+            foreach (var emp in sn)
+            {
+                Console.WriteLine($"ID : {emp.Employee_Id} Name : {emp.First_Name} {emp.Last_Name} Salary : {emp.Salary} Joining Date : {emp.Joining_Date} Department : {emp.Department}");
+            }
+            var yr = Employee.GetEmployees().Where(e => e.Joining_Date.Year == 2013);
+            foreach (var emp in yr)
+            {
+                Console.WriteLine($"ID : {emp.Employee_Id} Name : {emp.First_Name} {emp.Last_Name} Salary : {emp.Salary} Joining Date : {emp.Joining_Date} Department : {emp.Department}");
+            }
+            var mn = Employee.GetEmployees().Where(e => e.Joining_Date.Month == 01);
+            foreach (var emp in mn)
+            {
+                Console.WriteLine($"ID : {emp.Employee_Id} Name : {emp.First_Name} {emp.Last_Name} Salary : {emp.Salary} Joining Date : {emp.Joining_Date} Department : {emp.Department}");
+            }
+            var jan = Employee.GetEmployees().Where(e => e.Joining_Date < new DateTime(2013, 01, 01));
+            foreach (var emp in jan)
+            {
+                Console.WriteLine($"ID : {emp.Employee_Id} Name : {emp.First_Name} {emp.Last_Name} Salary : {emp.Salary} Joining Date : {emp.Joining_Date} Department : {emp.Department}");
+            }
+            var janAfter = Employee.GetEmployees().Where(e => e.Joining_Date > new DateTime(2013, 01, 01));
+            foreach (var emp in janAfter)
+            {
+                Console.WriteLine($"ID : {emp.Employee_Id} Name : {emp.First_Name} {emp.Last_Name} Salary : {emp.Salary} Joining Date : {emp.Joining_Date} Department : {emp.Department}");
+            }
+            var time = Employee.GetEmployees().Select(e => new { Joining_Date = e.Joining_Date.Day, Joining_Time = e.Joining_Date.TimeOfDay });
+            foreach (var emp in time)
+            {
+                Console.WriteLine($"Joining Date : {emp.Joining_Date} Time : {emp.Joining_Time}");
+            }
+            var mili = Employee.GetEmployees().Select(e => new { Joining_Date = e.Joining_Date, Joining_Time = e.Joining_Date.Millisecond });
+            foreach (var emp in mili)
+            {
+                Console.WriteLine($"Joining Date : {emp.Joining_Date} Time : {emp.Joining_Time}");
+            }
+            var last = Employee.GetEmployees().Where(e => e.Last_Name.Contains("%"));
+            foreach (var emp in last)
+            {
+                Console.WriteLine($"ID : {emp.Employee_Id} Name : {emp.First_Name} {emp.Last_Name} Salary : {emp.Salary} Joining Date : {emp.Joining_Date} Department : {emp.Department}");
+            }
+            var replace = Employee.GetEmployees().Select(e => e.Last_Name.Replace("%", " "));
+            foreach (var emp in replace)
+            {
+                Console.WriteLine($"Name : {emp} ");
+            }
+            var ts = Employee.GetEmployees().GroupBy(e => e.Department).Select(t => new { Total_Salary = t.Sum(e => e.Salary) });
+            foreach (var emp in ts)
+            {
+                Console.WriteLine($"Total Salary : {emp.Total_Salary} ");
+            }
+            var obts = Employee.GetEmployees().GroupBy(e => e.Department).Select(t => new { Total_Salary = t.Sum(e => e.Salary) }).OrderByDescending(a => a.Total_Salary);
+            foreach (var emp in obts)
+            {
+                Console.WriteLine($"Total Salary : {emp.Total_Salary} ");
+            }
+            var avgSalary = Employee.GetEmployees().GroupBy(e => e.Department).Select(t => new { Avg_Salary = t.Average(e => e.Salary) }).OrderBy(t => t.Avg_Salary);
+            foreach (var emp in avgSalary)
+            {
+                Console.WriteLine($"Average Salary of Department : {emp.Avg_Salary} ");
+            }
+            var maxSalary = Employee.GetEmployees().GroupBy(e => e.Department).Select(t => new { Max_Salary = t.Max(e => e.Salary) }).OrderBy(t => t.Max_Salary);
+            foreach (var emp in maxSalary)
+            {
+                Console.WriteLine($"Maximum Salary Department : {emp.Max_Salary} ");
+            }
+            var minSalary = Employee.GetEmployees().GroupBy(e => e.Department).Select(t => new { Min_Salary = t.Min(e => e.Salary) }).OrderBy(t => t.Min_Salary);
+            foreach (var emp in minSalary)
+            {
+                Console.WriteLine($"Minimum Salary : {emp.Min_Salary} ");
+            }
+            var ym = Employee.GetEmployees().GroupBy(a => new { Year = a.Joining_Date.Year, Month = a.Joining_Date.Month }).Select(t => new { No_of_Emp = t.Count() }).ToList();
+            foreach (var emp in ym)
+            {
+                Console.WriteLine($"No. of Employee : {emp.No_of_Emp}");
+            }
+            var tsa = Employee.GetEmployees().GroupBy(e => e.Department).Select(a => new { Total_Salary = a.Sum(e => e.Salary) }).Where(a => a.Total_Salary > 800000).OrderByDescending(a => a.Total_Salary);
+            foreach (var emp in tsa)
+            {
+                Console.WriteLine($"Total Salary  : {emp.Total_Salary}");
+            }
+            var top = Employee.GetEmployees().OrderByDescending(e => e.Salary).Take(2).ToList();
+            foreach (var emp in top)
+            {
+                Console.WriteLine($"Top 2 Salary : {emp.Salary} ");
+            }
+            var topn = Employee.GetEmployees().OrderByDescending(e => e.Salary).Take(5).ToList();
+            foreach (var emp in topn)
+            {
+                Console.WriteLine($"Top 5 Salary : {emp.Salary} ");
+            }
+            var top2s = Employee.GetEmployees().OrderByDescending(e => e.Salary).Skip(1).First();
+            Console.WriteLine($"2nd Highest Salary : {top2s.Salary} ");
+
             Console.ReadKey();
         }
     }
