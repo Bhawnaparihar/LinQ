ce6be7ed841decff4394944fff3a79bebf281cd7
Bhawna Parihar <52083996+Bhawnaparihar@users.noreply.github.com>
2020-05-05 17:46:51 +0530

    Add files via upload

---
+++ LinqAssignment/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAssignment
+{
+    class Employee
+    {
+        public int Employee_Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public double Salary { get; set; }
+        public System.DateTime Joining_Date { get; set; }
+        public string Department { get; set; }
+
+        public static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>
+            {
+            new Employee() {Employee_Id=1,First_Name="John",Last_Name="Abraham",Salary=1000000,Joining_Date= new DateTime(2013/01/1),Department="Banking"},
+            new Employee() {Employee_Id=2,First_Name="Michael",Last_Name="Clarke",Salary=800000,Joining_Date= new DateTime(2013/01/1),Department="Insurance"},
+            new Employee() {Employee_Id=3,First_Name="Roy",Last_Name="Thomas",Salary=700000,Joining_Date= new DateTime(2013/02/1),Department="Banking"},
+            new Employee() {Employee_Id=4,First_Name="Tom",Last_Name="Jose",Salary=600000,Joining_Date= new DateTime(2013/02/1),Department="Insurance"},
+            new Employee() {Employee_Id=5,First_Name="Jerry",Last_Name="Pinto",Salary=650000,Joining_Date= new DateTime(2013/02/1),Department="Insurance"},
+            new Employee() {Employee_Id=6,First_Name="Philip",Last_Name="Mathew",Salary=750000,Joining_Date= new DateTime(2013/01/1),Department="Services"},
+            new Employee() {Employee_Id=7,First_Name="TestName1",Last_Name="123",Salary=650000,Joining_Date= new DateTime(2013/01/1),Department="Services"},
+            new Employee() {Employee_Id=8,First_Name="TestName2",Last_Name="Lname%",Salary=600000,Joining_Date= new DateTime(2013/02/1),Department="Insurance"}
+            };
+            return employees;
+        }
+
+    }
+}
---
+++ LinqAssignment/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace LinqAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> basic =Employee.GetEmployees().ToList();
+            foreach(Employee emp in basic)
+            {
+                Console.WriteLine($"ID : {emp.Employee_Id} Name : {emp.First_Name} {emp.Last_Name} Salary : {emp.Salary} Joining Date : {emp.Joining_Date} Department : {emp.Department}");
+
+            }
+            List<Employee> select = Employee.GetEmployees().Select(emp => new Employee() { First_Name = emp.First_Name, Last_Name = emp.Last_Name }).ToList();
+            foreach(Employee emp in select)
+            {
+                Console.WriteLine($" Name : {emp.First_Name} {emp.Last_Name}  ");
+            }
+            List<Employee> alias = Employee.GetEmployees().Select(emp => new Employee() { First_Name = emp.First_Name }).ToList();
+            foreach (var emp in alias)
+            {
+                Console.WriteLine($" Employee Name : {emp.First_Name}  ");
+            }
+            string[] Upper=Employee.GetEmployees().Select(emp => emp.First_Name.ToUpper()).ToArray();
+            foreach (var emp in Upper)
+            {
+                Console.WriteLine($" Employee Name : {emp}  ");
+            }
+            string[] Lower = Employee.GetEmployees().Select(emp => emp.First_Name.ToLower()).ToArray();
+            foreach (var emp in Lower)
+            {
+                Console.WriteLine($" Employee Name : {emp}  ");
+            }
+            var unique = Employee.GetEmployees().Select(emp => emp.Department).Distinct().ToList();
+            foreach (var dept in unique)
+            {
+                Console.WriteLine($" Department : {dept}  ");
+            }
+           
+            string n = "John";
+            char[] textarray = n.ToCharArray();
+
+            for (int i = 1; i < n.Length; i++)
+            {
+                if (textarray[i].Equals('o'))
+                {
+                    Console.WriteLine("The letter is present in " + (i + 1) + "th position");
+                    break;
+                }
+
+            }
+            var trim = Employee.GetEmployees().Select(e => e.First_Name.TrimEnd()).ToList();
+            foreach (var emp in trim)
+            {
+                Console.WriteLine($" Employee Name : {emp}  ");
+            }
+            var triml = Employee.GetEmployees().Select(emp => emp.First_Name.TrimStart()).ToList();
+            foreach (var emp in triml)
+            {
+                Console.WriteLine($" Employee Name : {emp}  ");
+            }
+            var length = Employee.GetEmployees().Select(emp => emp.First_Name.Length).ToList();
+            foreach (var emp in length)
+            {
+                Console.WriteLine($" Employee Name : {emp}  ");
+            }
+            var relplace = Employee.GetEmployees().Select(emp => emp.First_Name.Replace("o", "$")).ToList();
+            foreach (var emp in relplace)
+            {
+                Console.WriteLine($" Employee Name : {emp}  ");
+            }
+            List<Employee> name = Employee.GetEmployees().Select(emp => new Employee() { First_Name = emp.First_Name, Last_Name = emp.Last_Name }).ToList();
+            foreach (var emp in name)
+            {
+                Console.WriteLine($" Name : {emp.First_Name}_{emp.Last_Name}  ");
+            }
+            var join = Employee.GetEmployees().Select(e => new { First_Name = e.First_Name, Joining_Year = e.Joining_Date.Year, Joining_Month = e.Joining_Date.Month, Joining_Date = e.Joining_Date.Day });
+            foreach (var emp in join)
+            {
+                Console.WriteLine($" Name : {emp.First_Name}  Joining Year : {emp.Joining_Year}  Joining Month : {emp.Joining_Month} Joining Day : {emp.Joining_Date}");
+            }
+            var asc = Employee.GetEmployees().OrderBy(emp => emp.First_Name).ToList();
+            foreach (var emp in asc)
+            {
+                Console.WriteLine($"Name : {emp.First_Name}");
+            }
+            var de = Employee.GetEmployees().OrderByDescending(emp => emp.First_Name).ToList();
+            foreach (var emp in de)
+            {
+                Console.WriteLine($"Name : {emp.First_Name}");
+            }
+            var all = Employee.GetEmployees().OrderBy(emp => emp.First_Name).ThenByDescending(s => s.Salary).ToList();
+            foreach (var emp in all)
+            {
+                Console.WriteLine($"Name : {emp.First_Name}  Salary : {emp.Salary}");
+            }
+            var ename = Employee.GetEmployees().Select(e => e.First_Name = "John");
+            foreach (var e in ename)
+            {
+                Console.WriteLine($"Name : {e}");
+            }
+            var ename2 = Employee.GetEmployees().Select(e => e.First_Name == "John" || e.First_Name == "Roy");
+            foreach (var e in ename2)
+            {
+                Console.WriteLine($"Name : {e}");
+            }
+            var ename3 = Employee.GetEmployees().Select(e => e.First_Name != "John" || e.First_Name != "Roy");
+            foreach (var e in ename3)
+            {
+                Console.WriteLine($"Name : {e}");
+            }
+            var ename4 = Employee.GetEmployees().Select(e => e.First_Name.StartsWith("J"));
+            foreach (var e in ename4)
+            {
+                Console.WriteLine($"Name : {e}");
+            }
+            var ename5 = Employee.GetEmployees().Select(e => e.First_Name.Contains("o"));
+            foreach (var e in ename5)
+            {
+                Console.WriteLine($"Name : {e}");
+            }
+            var ename6 = Employee.GetEmployees().Select(e => e.First_Name.EndsWith("n"));
+            foreach (var e in ename6)
+            {
+                Console.WriteLine($"Name : {e}");
+            }
+            var q = Employee.GetEmployees().Where(e => e.Salary > 600000).ToList();
+            foreach (var emp in q)
+            {
+                Console.WriteLine($"ID : {emp.Employee_Id} Name : {emp.First_Name} {emp.Last_Name} Salary : {emp.Salary} Joining Date : {emp.Joining_Date} Department : {emp.Department}");
+            }
+            var s = Employee.GetEmployees().Where(e => e.Salary < 800000).ToList();
+            foreach (var emp in s)
+            {
+                Console.WriteLine($"ID : {emp.Employee_Id} Name : {emp.First_Name} {emp.Last_Name} Salary : {emp.Salary} Joining Date : {emp.Joining_Date} Department : {emp.Department}");
+            }
+            var sa = Employee.GetEmployees().Where(e => e.Salary >= 500000 && e.Salary <= 800000);
+            foreach (var emp in q)
+            {
+                Console.WriteLine($"ID : {emp.Employee_Id} Name : {emp.First_Name} {emp.Last_Name} Salary : {emp.Salary} Joining Date : {emp.Joining_Date} Department : {emp.Department}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
